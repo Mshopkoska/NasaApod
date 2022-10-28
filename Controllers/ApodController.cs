b9d2b030f96aeb66b635b54c5fec764568189787
mimoza.shopkoska <mimoza.shopkoska@emitknowledge.com>
2022-10-28 12:33:58 +0200

    Add project files.

---
+++ Controllers/ApodController.cs
@@ -0,0 +1,40 @@
+﻿using APOD.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+namespace APOD.Controllers
+{
+    public class ApodController : Controller
+    {
+        private string _apiKey = "Xfwoi46lbj0tkkjmDrGt0f9uappcLhMjQhwDk0Mf";
+        private string _apodEndpoint = "https://api.nasa.gov/planetary/apod";
+        
+        
+       //https://localhost:7129/APOD/Get
+        [HttpPost]
+        public IActionResult Get(string date)
+        {
+            string endpoint = string.Empty;
+            bool isHD = false;
+
+            var data = Convert.ToDateTime(date);
+            endpoint = $"{this._apodEndpoint}?api_key={this._apiKey}&hd={isHD.ToString().ToLower()}&Date={data.ToString("yyyy-MM-dd")}";
+            using (var client = new HttpClient(new System.Net.Http.HttpClientHandler()))
+            {
+                var result = client.GetAsync(endpoint);
+
+                var resultToObject = result.Result.Content.ReadAsStringAsync();
+
+                var deserialized = JsonConvert.DeserializeObject<NasaApod>(resultToObject.Result.ToString());
+                if (deserialized.MediaType.Equals("video"))
+                {
+                    return View("GetVideo", deserialized);
+
+                }
+
+                return View("GetPhoto", deserialized);
+            }
+        }
+    }
+}
---
+++ Models/NasaApod.cs
@@ -0,0 +1,30 @@
+﻿namespace APOD.Models
+{
+    public class NasaApod
+    {
+        public string Copyright { get; set; }
+        public string Date { get; set; }
+        public string Explanation { get; set; }
+        public string HdUrl { get; set; }
+        public string MediaType { get; set; }
+        public string ServiceVersion { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public byte[] Image { get; set; }
+
+        public NasaApod(){}
+
+        public NasaApod(string copyright, string date, string explanation, string hdUrl, string mediaType, string serviceVersion, string title, string url, byte[] image)
+        {
+            this.Copyright = copyright;
+            this.Date = date;
+            this.Explanation = explanation;
+            this.HdUrl = hdUrl;
+            this.MediaType = mediaType;
+            this.ServiceVersion = serviceVersion;
+            this.Title = title;
+            this.Url = url;
+            this.Image = image;
+        }
+    }
+}
